ea468a4e4f15c5cd56f2f4b45d83b20ad41c5af1
Vincent A. Arcila L <vaarcilal@eafit.edu.co>
2019-04-30 14:29:41 -0500

    Falta abarcar caso en que hay una sola abeja en un cubo

--- src/spatialhashing.cpp
+++ src/spatialhashing.cpp
@@ -3,72 +3,50 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 class Bee {
 public:
     float x, y, z;
+    Bee* previous = nullptr;
+    Bee* following = nullptr;
+
     Bee(float x1, float y1, float z1)
     {
-        x = x1;
-        y = y1;
+        x = x1 * 111111;
+        y = y1 * 111111;
         z = z1;
     };
 
     Bee(const Bee& b)
     {
-        x = b.x;
-        y = b.y;
+        x = b.x * 111111;
+        y = b.y * 111111;
     };
 };
 
 class Cube {
 public:
     int x, y, z;
-    Cube* previous = nullptr;
-    Cube* following = nullptr;
-
     Cube(int x, int y, int z)
     {
-        this->x = x;
-        this->y = y;
+        this->x = x * 111111;
+        this->y = y * 111111;
         this->z = z;
     }
 };
 
-class CubeKey {
-public:
-    int x, y, z, counter;
-
-    CubeKey(int x1, int y1, int z1)
-    {
-        x = x1;
-        y = y1;
-        z = z1;
-        counter = 0;
-    }
-
-    CubeKey(int x1, int y1, int z1, int counter)
-    {
-        {
-            x = x1;
-            y = y1;
-            z = z1;
-            counter = counter;
-        }
-    }
-};
-
-int find_cube_key(Cube* c)
+std::string find_cube_key(float x, float y, float z)
 {
-    // round each coordinate, concatenate them into a string anf convert this string to a integer
-    return std::stoi(std::to_string(std::round(c->x / 57.735026919d)) + std::to_string(std::round(c->y / 57.735026919d)) + std::to_string(round(c->z / 57.735026919d)));
+    // round each coordinate, concatenate them into a string and convert this string to an integer
+    return std::to_string((int)std::round(x / 57.735026919d)) + " " + std::to_string((int)std::round(y / 57.735026919d)) + " " + std::to_string((int)std::round(z / 57.735026919d));
 }
 
-boost::unordered_map<int, std::pair<Cube*, bool>> parse_file(std::string input_file, std::vector<int>& v)
+boost::unordered_map<std::string, std::pair<Bee*, bool>> parse_file(std::string input_file, std::vector<std::string>& v)
 {
-    boost::unordered_map<int, std::pair<Cube*, bool>> um;
+    boost::unordered_map<std::string, std::pair<Bee*, bool>> um;
     std::ifstream inpp(input_file);
     std::string line;
     std::getline(inpp, line);
@@ -76,8 +54,8 @@ boost::unordered_map<int, std::pair<Cube*, bool>> parse_file(std::string input_f
         while (std::getline(inpp, line)) {
             std::vector<std::string> string_coordinates;
             boost::split(string_coordinates, line, boost::is_any_of(","));
-            Cube* c = new Cube(std::stoi(string_coordinates[0]), std::stoi(string_coordinates[1]), std::stoi(string_coordinates[2]));
-            int key = find_cube_key(c);
+            Bee* c = new Bee(std::stof(string_coordinates[0]), std::stof(string_coordinates[1]), std::stof(string_coordinates[2]));
+            std::string key = find_cube_key(c->x, c->y, c->z);
             if ((um[key].first) == nullptr) {
                 um[key] = std::make_pair(c, false);
                 v.push_back(key);
@@ -93,22 +71,34 @@ boost::unordered_map<int, std::pair<Cube*, bool>> parse_file(std::string input_f
     return um;
 }
 
+
+
 int main()
 {
     // map trepresent cubes
 
     // vector to store keys and int representing how many bees are in each cube
-    std::vector<int> keys;
+    std::vector<std::string> keys;
 
     // string storing name of file that will be parsed
     std::string inFileName = "ConjuntoDeDatosCon1000abejas.txt";
-    boost::unordered_map<int, std::pair<Cube*, bool>> cubes = parse_file(inFileName, keys);
+    boost::unordered_map<std::string, std::pair<Bee*, bool>> cubes = parse_file(inFileName, keys);
+    std::stringstream s;
 
-    for (int i : keys) {
-        Cube* tony = cubes[i].first;
-        do {
-            std::cout << tony->x << "  " << tony->y << "  " << tony->z << std::endl;
-            tony = tony->following;
-        } while (tony->following != nullptr);
+    std::vector<std::string> xyz_from_key;
+
+    for (std::string i : keys) {
+        std::pair<Bee*, bool> p = cubes[i];
+        Bee* tony = p.first;
+        if (p.second == true) {
+            do {
+                // s << tony->x << "," << tony->y << "," << tony->z << std::endl;
+                tony = tony->following;
+            } while (tony->following != nullptr);
+        } else {
+            boost::split(xyz_from_key, i, boost::is_any_of(" "));
+
+        }
     }
+    std::cout << s.str();
 }
